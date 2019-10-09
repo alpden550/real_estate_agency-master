c99f880e5475a97d29a52ae489e63e2c1b9c199d
alpden550 <alpden550@gmail.com>
2019-10-09 16:06:53 +0300

    Added data-migrations.
    Added filter for admin.
    
    Added new field new_building and created it editable.

--- property/admin.py
+++ property/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 from .models import Flat
 
 
-admin.site.register(Flat)
+@admin.register(Flat)
+class FlatAdmin(admin.ModelAdmin):
+    search_fields = ('owner', 'town', 'address')
+    readonly_fields = ('created_at',)
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
+    list_editable = ('new_building',)
+    list_filter = ('new_building', 'construction_year')
